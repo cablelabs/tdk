c20d0d09709b5d2c1db43ad4660914cc088358d9
Jim Lawton <jim.lawton@s3group.com>
2014-11-25 12:44:46 +0000

    RDKCMF-623  Update from Comcast source branch master.
    Change-Id: I8aa57c75a0bb2aa4fbc84a3e6472f6e82d080a56
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/devices/intel-x86-pc/rdkemulator
    SHA1: ad8603de9ede493fccdc35c20a7683dc3379291a

--- agent/scripts/StopTDK.sh
+++ agent/scripts/StopTDK.sh
@@ -3,9 +3,9 @@ echo "Stopping TDK Agent.."
 sleep 1
 
 #Killing inactive TDK processes
-ps | grep "agent" | grep -v "grep" | grep -v "tcf-agent" | awk '{print $1}' | xargs kill -9 >& /dev/null
-ps | grep "tftp" | grep -v "grep" | awk '{print $1}' | xargs kill -9 >& /dev/null
-ps | grep "/opt/TDK/" | grep -v "grep" | awk '{print $1}' | xargs kill -9 >& /dev/null
+ps -ef | grep "agent" | grep -v "grep" | grep -v "tr69agent" | awk '{print $2}' | xargs kill -9 >& /dev/null
+ps -ef | grep "tftp" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
+ps -ef | grep "/opt/TDK/" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 sleep 2
 
 echo "Done"
