318b2a20e905c3a621d766edb2190e6918f8d75c
Jim Lawton <jim.lawton@s3group.com>
2015-12-09 10:24:16 +0000

    RDKCMF-2649  Update from Comcast source (master).
     * Fix for the killing issue of TDKAgent
    
    Change-Id: Id3fa12b7ac0c9106d3bc149747ddf50807eb03e6
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/devices/intel-x86-pc/rdkemulator
    SHA1: 927ea1aea2fa2a05340f0a3d5e68bea08eaa9b24

--- agent/scripts/StopTDK.sh
+++ agent/scripts/StopTDK.sh
@@ -16,6 +16,8 @@ export TDK_PATH=/opt/TDK
 sleep 1
 
 #Killing inactive TDK processes
+ps -ef | grep "TDKagentMonitor" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
+sleep 1
 ps -ef | grep "tdk_agent" | grep -v "grep" | grep -v "tr69agent" | awk '{print $2}' | xargs kill -9 >& /dev/null
 ps -ef | grep "tftp" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 ps -ef | grep $TDK_PATH | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
