86c50b37b2c55e77fb4d8ee330cafdd22e5e3999
Jim Lawton <jim.lawton@s3group.com>
2016-02-25 06:11:58 +0000

    RDKCMF-3052  Update from Comcast source (master).
     * Updated the Recorder_testmodule_pre-script.sh
    
    Change-Id: I53850f4621f13bccae141dce0ebe3375748c0598
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/devices/intel-x86-pc/rdkemulator
    SHA1: 30946de2dedb5adab749609f49b67005be46616f

--- Recorder_stub/scripts/Recorder_testmodule_pre-script.sh
+++ Recorder_stub/scripts/Recorder_testmodule_pre-script.sh
@@ -3,68 +3,101 @@ TARGET_PATH=/opt/
 #removing old configuring status from the opt
 rm $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
 
-touch /tmp/stage4
-touch /tmp/stt_received
-
-#Copying the rmfconfig.ini file in to /opt/
-ls $TARGET_PATH/rmfconfig.ini
-if [ $? == 0 ]; then
-        echo "file present"
-else
-	if cp $RMFCONFIG_PATH/rmfconfig.ini $TARGET_PATH; then
-	   echo "rmfconfig.ini is copied from " $RMFCONFIG_PATH
-	else
-	   echo "rmfconfig.ini is not copied from " $RMFCONFIG_PATH 1>&2
-	   touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
-	   echo "FAILURE<DETAILS>Unable to copy rmfconfig.ini" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
-	   exit 1
-	fi
-fi
-
-#Editing the rmf_config.ini to disbale the dtcp flag
+#Editing the rmf_config.ini to change the server URL
 rmf_flags="FEATURE.LONGPOLL.URL"
-FLAG_SEARCH_STATUS=`grep $rmf_flags $TARGET_PATH/rmfconfig.ini|wc -l`
+IP=$(awk -F"@" '/Manager IP/{ip=$2}END{print ip}' $TDK_PATH/tdkconfig.ini )
+echo $IP
+FLAG_SEARCH_STATUS=`grep $rmf_flags=http://$IP:8080/DVRSimulator/longpollServer $RMFCONFIG_PATH/rmfconfig.ini|wc -l`
 if [ $FLAG_SEARCH_STATUS -ne 0 ]
 then
-     echo $rmf_flags is found in rmfconfig.ini
-     #Editing the flag to false
-     IP=$(awk -F"@" '/Manager IP/{ip=$2}END{print ip}' $TDK_PATH/tdkconfig.ini )
-     echo $IP
-     sed -i -e "s#$rmf_flags=.*#$rmf_flags=http://$IP:8000/longpollServer#g" $TARGET_PATH/rmfconfig.ini
+     echo $rmf_flags "is found in rmfconfig.ini"
+else
+     #Editing the rmf_config.ini to change the server URL
+     sed -i -e "s#$rmf_flags=.*#$rmf_flags=http://$IP:8080/DVRSimulator/longpollServer#g" $RMFCONFIG_PATH/rmfconfig.ini
      if [ $? -eq 0 ]
      then
-        echo "rmfconfig.ini file edited"
-	touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
-        echo  "SUCCESS" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+        echo "rmfconfig.ini file edited for" $rmf_flags
+        REBOOT="TRUE"
      else
         echo "sed utillity is not found"
         touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
         echo  "FAILURE<DETAILS>sed utillity is not found" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
         exit 1
      fi
-else
-     echo $rmf_flags "is not found in rmfconfig.ini"
-     touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
-     echo  "FAILURE<DETAILS>" $rmf_flags "is not found in rmfconfig.ini" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
-
 fi
 
-#Restart the rmfstreamer,vodclient,podmanager & snmp
-#process_name="rmf-streamer vod-service pod-service snmp-manager-service"
-#for process in $process_name
-#do
-#        /etc/init.d/$process restart
-#        if [ $? -eq 0 ]
-#        then
-##                echo $process "restarted"
-#                touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
-#                echo  "SUCCESS" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
-#		
-#        else
-#                echo $process "not able to restart"
-##                touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
- #               echo  "FAILURE<DETAILS>" $process "not restarted" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
- #       fi
-#done
+rmf_flags="FEATURE.RWS.POST.URL"
+FLAG_SEARCH_STATUS=`grep $rmf_flags=http://$IP:8080/DVRSimulator/recorder/status $RMFCONFIG_PATH/rmfconfig.ini|wc -l`
+if [ $FLAG_SEARCH_STATUS -ne 0 ]
+then
+     echo $rmf_flags "is found in rmfconfig.ini"
+else
+     #Editing the rmf_config.ini to change the server URL
+     IP=$(awk -F"@" '/Manager IP/{ip=$2}END{print ip}' $TDK_PATH/tdkconfig.ini )
+     echo $IP
+     sed -i -e "s#$rmf_flags=.*#$rmf_flags=http://$IP:8080/DVRSimulator/recorder/status#g" $RMFCONFIG_PATH/rmfconfig.ini
+     if [ $? -eq 0 ]
+     then
+        echo "rmfconfig.ini file edited for" $rmf_flags
+        REBOOT="TRUE"
+     else
+        echo "sed utillity is not found"
+        touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
+        echo  "FAILURE<DETAILS>sed utillity is not found" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+        exit 1
+     fi
+fi
 
+rmf_flags="FEATURE.RWS.GET.URL"
+FLAG_SEARCH_STATUS=`grep $rmf_flags=http://$IP:8080/DVRSimulator/recorder/updateSchedule $RMFCONFIG_PATH/rmfconfig.ini|wc -l`
+if [ $FLAG_SEARCH_STATUS -ne 0 ]
+then
+     echo $rmf_flags is found in rmfconfig.ini
+else
+     #Editing the rmf_config.ini to change the server URL
+     IP=$(awk -F"@" '/Manager IP/{ip=$2}END{print ip}' $TDK_PATH/tdkconfig.ini )
+     echo $IP
+     sed -i -e "s#$rmf_flags=.*#$rmf_flags=http://$IP:8080/DVRSimulator/recorder/updateSchedule#g" $RMFCONFIG_PATH/rmfconfig.ini
+     if [ $? -eq 0 ]
+     then
+        echo "rmfconfig.ini file edited for" $rmf_flags
+        REBOOT="TRUE"
+     else
+        echo "sed utillity is not found"
+        touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
+        echo  "FAILURE<DETAILS>sed utillity is not found" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+        exit 1
+     fi
+fi
 
+rmf_flags="FEATURE.LONGPOLL.END.POINT"
+FLAG_SEARCH_STATUS=`grep $rmf_flags=http://$IP:8080/DVRSimulator/longpollEndPoint $RMFCONFIG_PATH/rmfconfig.ini|wc -l`
+if [ $FLAG_SEARCH_STATUS -ne 0 ]
+then
+     echo $rmf_flags is found in rmfconfig.ini
+else
+     #Editing the rmf_config.ini to change the server URL
+     IP=$(awk -F"@" '/Manager IP/{ip=$2}END{print ip}' $TDK_PATH/tdkconfig.ini )
+     sed -i "/FEATURE.LONGPOLL.URL/ a $rmf_flags=http://$IP:8080/DVRSimulator/longpollEndPoint" $RMFCONFIG_PATH/rmfconfig.ini
+     if [ $? -eq 0 ]
+     then
+        echo "rmfconfig.ini file edited"
+        REBOOT="TRUE"
+     else
+        echo "sed utillity is not found"
+        touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
+        echo  "FAILURE<DETAILS>sed utillity is not found" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+        exit 1
+     fi
+fi
+
+if [[ $REBOOT == "TRUE" ]]
+then
+     touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
+     echo  "<REBOOT> : rmfconfig.ini is changed" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+     echo  "<REBOOT> : rmfconfig.ini is changed"
+else
+     touch $TDK_PATH/logs/Recorder_testmodule_prereq_details.log;
+     echo  "SUCCESS<DETAILS>" $rmf_flags "is already set to appropriate server" >> $TDK_PATH/logs/Recorder_testmodule_prereq_details.log
+     echo  "SUCCESS<DETAILS>" $rmf_flags "is already set to appropriate server"
+fi     
